abc0f0769af521a5afff568fa2c889584cbb6ca6
Sara Vargas <saravargas864@gmail.com>
2024-11-03 19:51:43 -0500

    conexión frontend publicado

--- src/config/Axios.jsx
+++ src/config/Axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
+// Configuración de la instancia de Axios con la URL base desde variables de entorno
 const axiosconexion = axios.create({
-        baseURL: "https://rentaavalla.somee.com/api",
+    baseURL: process.env.REACT_APP_API_URL, // Variable de entorno para la URL de la API
 });
 
 export default axiosconexion;
-
