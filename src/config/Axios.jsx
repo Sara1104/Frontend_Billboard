8bd3cab48c527de147d93a2d372e19e9308e4f34
Sara Vargas <saravargas864@gmail.com>
2024-11-04 00:17:18 -0500

    arreglar conexión con el backend2

--- src/config/Axios.jsx
+++ src/config/Axios.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Configuración de la instancia de Axios con la URL base desde variables de entorno
 const axiosconexion = axios.create({
-        baseURL: "https://rentavalla.somee.com/api",
-        //baseURL: process.env.REACT_APP_API_URL, // Variable de entorno para la URL de la API
+        //baseURL: "https://rentavalla.somee.com/api",
+        baseURL: process.env.REACT_APP_API_URL, // Variable de entorno para la URL de la API
 
         headers: {
                 'Content-Type': 'application/json',
