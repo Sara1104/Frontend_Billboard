fcd0a86012458915d22f63dfbe1e2aee7edded59
Sara Vargas <saravargas864@gmail.com>
2024-11-04 01:48:38 -0500

    Intento de conexión Backend 1

--- src/config/Axios.jsx
+++ src/config/Axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+console.log("API URL:", process.env.REACT_APP_API_URL);
 // Configuración de la instancia de Axios con la URL base desde variables de entorno
 const axiosconexion = axios.create({
         //baseURL: "https://rentavalla.somee.com/api",
